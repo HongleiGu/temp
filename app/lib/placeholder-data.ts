57c1e7172ef63858a415f7a5faa59af4545febae
AnishKochhar <anish@kochhars.com>
2024-09-06 18:04:59 +0100

    Add: Simple placeholder data

---
+++ app/lib/placeholder-data.ts
@@ -0,0 +1,27 @@
+
+const users = [
+    {
+        id: '410544b2-4001-4271-9855-fec4b6a6442a',
+        name: 'User',
+        email: 'user@nextmail.com',
+        password: '123456',
+    },
+];
+
+
+const events = [
+    {
+        id: '410544b2-4001-4271-9855-fec4b6a6442a',
+        organiser_name: 'Imperial Neurotech Society',
+        event_title: 'Andreas Shaefer - From X-Rays to Brain Computer Interfaces',
+        description: 'Join us to hear all things Neurotechnology',
+        date: '2024-09-20',
+        time_start: '18:30',
+        time_end: '19:30',
+        location: 'Imperial College London, RSM Building, Room 1.31',
+        image: '/images/placeholders/room.png',
+    }
+]
+
+
+export { users, events }
